5663a5a8dcb3b7dd43fe221b0d0f3b0ad5c84b5d
maureen <maureen.murimi@moringaschool.com>
2022-05-14 12:30:07 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(listOfDrivers, searchedDriver){
+  return listOfDrivers.filter( driverName => {  
+      return driverName.toLowerCase() === searchedDriver.toLowerCase() 
+    });
+}
+
+
+function fuzzyMatch(listOfDrivers, matchingLetters){
+  return listOfDrivers.filter( driverName => {
+      return driverName.substring(0, 2) === matchingLetters 
+    });
+}
+
+function matchName(listOfDrivers, searchedDriver){
+  return listOfDrivers.filter( driverName => { 
+    return driverName.name === searchedDriver 
+  })
+}
